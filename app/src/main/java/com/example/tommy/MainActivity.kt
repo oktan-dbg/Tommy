3428e3a48e675a8133d9faddfdc64134ac9716b3
Oktan <107392424+octan-nautil@users.noreply.github.com>
2022-12-01 08:31:21 +0100

    Wykonanie layoutu

--- app/src/main/java/com/example/tommy/MainActivity.kt
+++ app/src/main/java/com/example/tommy/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.tommy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TableLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tablica = findViewById<TableLayout>(R.id.tablica_miasta);
+
+
+
+        tablica.addView(row1);
     }
 }
